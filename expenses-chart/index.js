2bfbd06548746f117c9cc3ca796f704ba7118506
qirele <kirilczikzik@gmail.com>
2022-07-07 21:04:52 +0200

    Tried my best today

--- expenses-chart/index.js
+++ expenses-chart/index.js
@@ -1,9 +1,19 @@
+const daysWrapper = document.querySelector('.days');
+
 fetch('data.json')
   .then(json => json.json())
   .then(data => {
     const maxAmount = Math.max(...data.map(el => el.amount));
-
+    let html = '';
     data.forEach(entry => {
-      console.log(`day: ${entry.day}, amount: ${entry.amount}`);
+      html += 
+      `
+        <div class="day">
+          <div class="amount">${entry.amount}</div>
+          <div class="graph">bruh</div>
+          <div class="nameOfDay">${entry.day}</div>
+        </div> 
+      `;
     });
+    daysWrapper.innerHTML = html;
   });
