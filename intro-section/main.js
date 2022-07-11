ab949522f562b59db490f2ebfba18ffb4b37d099
qirele <kirilczikzik@gmail.com>
2022-07-11 16:26:03 +0200

    finished hamburger menu functionality

---
+++ intro-section/main.js
@@ -0,0 +1,9 @@
+const burgerBtn = document.querySelector('.menu-btn');
+const list = document.querySelector('header ul');
+const darkenActive = document.querySelector('.darkenPage');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('open');
+  list.classList.toggle('active');
+  darkenActive.classList.toggle('active');
+});
