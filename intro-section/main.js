e9711f5b7a90d8988dbfd27027558119743a8667
qirele <kirilczikzik@gmail.com>
2022-07-12 20:13:43 +0200

    images are hard

--- intro-section/main.js
+++ intro-section/main.js
@@ -1,7 +1,7 @@
 const burgerBtn = document.querySelector(".menu-btn");
 const list = document.querySelector(".navigation");
 const darkenActive = document.querySelector(".darkenPage");
-let firstTwo = document.querySelectorAll(".hasDropdown");
+let firstTwo = document.querySelectorAll(".navigation > li:nth-child(-n + 2)");
 
 burgerBtn.addEventListener("click", () => {
   burgerBtn.classList.toggle("open");
@@ -11,12 +11,13 @@ burgerBtn.addEventListener("click", () => {
 
 
 firstTwo.forEach(el => {
-  el.addEventListener('click', () => {
-    let img = el.querySelector("img");
-    if (img.src.includes("down")) {
-      img.src = "./images/icon-arrow-up.svg";
+  const label = el.querySelector("label");
+  const checkbox = el.querySelector("input");
+  label.addEventListener('click', () => {
+    if (checkbox.checked) {
+      el.querySelector('img').src = "./images/icon-arrow-down.svg";
     } else {
-      img.src = "./images/icon-arrow-down.svg";
+      el.querySelector('img').src = "./images/icon-arrow-up.svg";
     }
   });
 });
