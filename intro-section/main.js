330bcc5684b058f32e87b4e2c5de1415ba68ffcd
qirele <kirilczikzik@gmail.com>
2022-07-12 14:29:33 +0200

    mobile navigation complete :O

--- intro-section/main.js
+++ intro-section/main.js
@@ -1,9 +1,23 @@
-const burgerBtn = document.querySelector('.menu-btn');
-const list = document.querySelector('.navigation');
-const darkenActive = document.querySelector('.darkenPage');
+const burgerBtn = document.querySelector(".menu-btn");
+const list = document.querySelector(".navigation");
+const darkenActive = document.querySelector(".darkenPage");
+let firstTwo = document.querySelectorAll(".hasDropdown");
 
-burgerBtn.addEventListener('click', () => {
-  burgerBtn.classList.toggle('open');
-  list.classList.toggle('active');
-  darkenActive.classList.toggle('active');
+burgerBtn.addEventListener("click", () => {
+  burgerBtn.classList.toggle("open");
+  list.classList.toggle("active");
+  darkenActive.classList.toggle("active");
+});
+
+
+firstTwo.forEach(el => {
+  el.addEventListener('click', () => {
+    console.log("click");
+    let img = el.querySelector("img");
+    if (img.src.includes("down")) {
+      img.src = "./images/icon-arrow-up.svg";
+    } else {
+      img.src = "./images/icon-arrow-down.svg";
+    }
+  });
 });
